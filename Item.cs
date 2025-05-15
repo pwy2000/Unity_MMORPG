03006446d7d101e6a96d12fb74c52db73d7625fa
pwy2000 <122452715+pwy2000@users.noreply.github.com>
2025-05-16 03:34:36 +0800

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,22 @@
+﻿using Common.Data;
+using SkillBridge.Message;
+using System.Linq;
+namespace Models
+{
+    public class Item
+    {
+        public int Id;
+        public int Count;
+        public ItemDefine Define;
+        public Item(NItemInfo item)
+        {
+            this.Id = item.Id;
+            this.Count = item.Count;
+            this.Define = DataManager.Instance.Items[item.Id];
+        }
+        public override string ToString()
+        {
+            return string.Format("Id:{0},Count:{1}", this.Id, this.Count);
+        }
+    }
+}
